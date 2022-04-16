ffb639fac28eb56d7bbde966d6332aac7dc8e8c6
Hunter Miller <hunter@hmiller.dev>
2022-04-16 00:45:34 -0500

    fixing build

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,12 +1,6 @@
 import { loadEnv, defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { urbitPlugin } from '@urbit/vite-plugin-urbit';
-// yarn add --dev @esbuild-plugins/node-globals-polyfill
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-// yarn add --dev @esbuild-plugins/node-modules-polyfill
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-// You don't need to add this to deps, it's included by @esbuild-plugins/node-modules-polyfill
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
@@ -15,31 +9,6 @@ export default ({ mode }) => {
   console.log(SHIP_URL);
 
   return defineConfig({
-    plugins: [urbitPlugin({ base: 'silo', target: SHIP_URL, secure: false }), reactRefresh()],
-    optimizeDeps: {
-      esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-            global: 'globalThis'
-        },
-        // Enable esbuild polyfill plugins
-        plugins: [
-            NodeGlobalsPolyfillPlugin({
-                process: true,
-                buffer: true
-            }),
-            NodeModulesPolyfillPlugin()
-        ]
-      }
-    },
-    build: {
-      rollupOptions: {
-        plugins: [
-          // Enable rollup polyfills plugin
-          // used during production bundling
-          rollupNodePolyFill()
-        ]
-      }
-    }
+    plugins: [urbitPlugin({ base: 'silo', target: SHIP_URL, secure: false }), reactRefresh()]
   });
 };
